241acf9b4f06b006c85145e14a588a5ac62fc527
sol1230 <solllll1230@gmail.com>
2023-01-06 14:53:23 +0900

    first commit

---
+++ src/main/java/com/mango/finalproject_mango/FinalprojectMangoApplication.java
@@ -0,0 +1,13 @@
+package com.mango.finalproject_mango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectMangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectMangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mango/finalproject_mango/FinalprojectMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.finalproject_mango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectMangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

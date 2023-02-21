b35f40a11ed6a1c71a25dc889829ef9f161299e7
sohiekim65 <sohiekim65@gmail.com>
2023-02-21 15:09:00 +0900

    controller modify

---
+++ src/main/java/com/mango/finalproject_mango/Controller/AdminBoardController.java
@@ -0,0 +1,18 @@
+package com.mango.finalproject_mango.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminBoardController {
+
+    // 답변등록
+    @RequestMapping(value = "/admin/admin_boards_qna", method = RequestMethod.GET)
+    public ModelAndView adminQNA(ModelAndView modelAndView) {
+        
+        return modelAndView;
+    }
+}
+

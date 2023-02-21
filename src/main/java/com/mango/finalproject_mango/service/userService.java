094e6208b8ae2d1278e4227ed21ab4eaa18d880f
ParkChanhee1130 <kkazza1130@gmail.com>
2023-02-21 22:29:33 +0900

    cm

--- src/main/java/com/mango/finalproject_mango/service/userService.java
+++ src/main/java/com/mango/finalproject_mango/service/userService.java
@@ -1,9 +1,13 @@
 package com.mango.finalproject_mango.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.mango.finalproject_mango.dao.CommonDao;
 
 @Service
 public class userService {
 
+    @Autowired
+    CommonDao commonDao;
     
 }
